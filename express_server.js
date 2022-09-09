c1b3712e83cb436221a24a9f3367ed199cfd2bfa
shtyagi22 <sh.tyagi22@gmail.com>
2022-09-08 23:57:08 -0400

    stored post request data in database and added new routes

--- express_server.js
+++ express_server.js
@@ -33,15 +33,31 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  // Respond with 'Ok' (we will replace this)
+
+  const shortURL = Math.random().toString(36).substring(2, 8);
+
+  urlDatabase[shortURL] = req.body.longURL;
+
+  res.redirect(urlDatabase[shortURL]);
+  res.end();
+
+});
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[shortURL];
+  res.redirect(longURL);
+});
+
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
-});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
